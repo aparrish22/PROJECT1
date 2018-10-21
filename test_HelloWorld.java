d0435b503bac41cda8743af53ed5a1e3c22e37df
Austin Parrish <42010578+aparrish22@users.noreply.github.com>
2018-10-21 17:41:39 -0500

    Update test_HelloWorld.java

--- test_HelloWorld.java
+++ test_HelloWorld.java
@@ -1,7 +1,9 @@
 public class test {
 
   public static void main(String args[]) {
-    System.out.print("Hello World test!");
+    System.out.print("Hello World test!\n");
+    
+    System.out.println("Another test!");
   }
 
 }
