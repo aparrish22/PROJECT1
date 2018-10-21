45e19c4914ed604f1726820f102be8b2e980d8f1
Austin Parrish <42010578+aparrish22@users.noreply.github.com>
2018-10-21 17:52:24 -0500

    Create BSTNode.java

---
+++ BSTNode.java
@@ -0,0 +1,37 @@
+public class BSTNode<T> {
+	T info;
+	BSTNode<T> left, right;
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public BSTNode<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BSTNode<T> left) {
+		this.left = left;
+	}
+
+	public BSTNode<T> getRight() {
+		return right;
+	}
+
+	public void setRight(BSTNode<T> right) {
+		this.right = right;
+	}
+
+	public BSTNode(T info, BSTNode<T> left,
+			BSTNode<T> right) {
+		super();
+		this.info = info;
+		this.left = left;
+		this.right = right;
+	}
+
+}
