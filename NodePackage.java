c925119d154df85fe89667bc0939e8485804ee95
cshain <40579596+cshain@users.noreply.github.com>
2018-10-22 17:28:52 -0500

    Update NodePackage.java
    Added descriptive comment

--- NodePackage.java
+++ NodePackage.java
@@ -1,3 +1,7 @@
+/* This class stores a node and the number of nodes passed through to find the node,
+ * so our find methods can return both the node it found and the number of comparisons
+ * made
+ */
 package project;
 
 public class NodePackage {
