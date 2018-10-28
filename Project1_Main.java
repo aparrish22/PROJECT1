493f949447687ea015848749b36ed20eb1c6d9b1
Austin Parrish <42010578+aparrish22@users.noreply.github.com>
2018-10-28 18:34:01 -0500

    Update Project1_Main.java

--- Project1_Main.java
+++ Project1_Main.java
@@ -1,17 +1,5 @@
 /* 
- * TODO
- * *** new parameter in find(NodePackage _)
- * NodePackage.java class will have 2 variables, the Node and an interger (will pass the size into it)
- * to indicate how many nodes the node has passed through
- *  
- * &
- * minID & maxID
- * 
- * 
- * 
- * TODO
- * Atici wants us to be able to pass T1 or T2
- * Like int minID(Project1 tree) then do tree.min().getID();
+ *
  */
 import java.util.Collection;
 import java.util.Iterator;
