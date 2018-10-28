e4bce1eb3ef23f250d82bf32ac732175194b7043
cshain <40579596+cshain@users.noreply.github.com>
2018-10-28 17:52:04 -0500

    Update Project1_Main.java

--- Project1_Main.java
+++ Project1_Main.java
@@ -17,60 +17,13 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public class Project1_Main<T> {
-    private BinarySearchTree<Node> BST;
-    private AVLTree AVL;
-
-    public Project1_Main() {
-        super();
-        BST = new BinarySeacrhTree<>();
-        AVL = new AVLTree();
-    }
-
-    // project find method, not BST or AVL find
-    // will call find from respective tree
-    // note: AVL: find(int id)
-    public int find(Object tree, int id) {
-        NodePackage nodepack = tree.find(id);
-        if (node == null)
-			return null;
-		else 
-			return node.getNode().getID();
-    }
-
-    public Node minID(Object tree) {
-        return tree.min();
-    }
-
-    public Node maxID(Object tree) {
-        return tree.max();
-    }
-
     public static void main(String args[]) {
-        BinarySearchTree<Node> T1 = new BinarySearchTree<>();
-        
-
-        int amountOfStudents = 10;
-        // inserting sample students
-        for (int i = 5; i < amountOfStudents; i++) {
-            Node student = new Node(i, "name", 4, null);
-            T1.insert(student);
-        }
-
-        for (int i = 4; i >= 0; i--) {
-            Node student = new Node(i, "name", 4, null);
-            T1.insert(student);
-        }
-
-        System.out.print(T1.inOrder());
-
-        
-
-
-        // Iterator it = T1.iterator();
+	BinarySearchTree<Node> BST = new BinarySearchTree<>();
+	AVLTree AVL = new AVLTree();
+	TreeManager trees = new TreeManager();
 		
-		// while (it.hasNext())
-		// 	System.out.println(it.next());
-
+	//Build the trees
+	//TODO Read the file
     }
 
 }
